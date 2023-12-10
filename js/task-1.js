f1998f55bfee77fbb4d6d2b1ca6afbb5133c062b
Dmytro DV <abbott_nutrition@ukr.net>
2023-12-10 18:42:05 +0200

    add tasks 2,3

--- js/task-1.js
+++ js/task-1.js
@@ -5,7 +5,7 @@ function isEnoughCapacity(products, containerSize) {
     let totalQuantity = 0;
 
     for (const product in products) {
-        totalQuantity += products[product];
+    totalQuantity += products[product];
     }
 
     return totalQuantity < containerSize ? true : false;
